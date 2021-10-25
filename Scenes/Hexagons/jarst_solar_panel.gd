b003157f0f8e7d61eff87a1b18e3889653719248
Kjarrigan <github@kjarrigan.de>
2021-10-25 21:43:01 +0200

    Cleanup: Solarpanel

--- Scenes/Hexagons/jarst_solar_panel.gd
+++ Scenes/Hexagons/jarst_solar_panel.gd
@@ -2,16 +2,13 @@ extends Spatial
 
 var active = true
 
-onready var light = $SpotLight
-
 func enable_light():
   active = true
-  light.light_color = Color(0, 255, 0)
-
+  $SpotLight.light_color = Color(0, 255, 0)
 
 func disable_light():
   active = false
-  light.light_color = Color(255, 0, 0)
+  $SpotLight.light_color = Color(255, 0, 0)
 
 func _process(_delta):
   if Statistics.night == true and active == true:
