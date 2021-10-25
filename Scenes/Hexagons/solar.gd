0264026a40d10c3bbb4af7ed4e6341e51ac799f7
Kjarrigan <github@kjarrigan.de>
2021-10-25 23:26:41 +0200

    Cleanup: UI (WIP)

--- Scenes/Hexagons/solar.gd
+++ Scenes/Hexagons/solar.gd
@@ -4,10 +4,8 @@ onready var influence_range = $influence_range
 
 var hovered = false
 
-
 func tick():
   pass
-
 
 func houses():
   var surrounding_houses = 0
@@ -16,19 +14,16 @@ func houses():
       surrounding_houses += 1
   return surrounding_houses
 
-
 func set_hover_text():
   Statistics.description_height = 100
   Statistics.description_text = \
     "Basic Solar Farm \n\n" \
     + "Serves " + String(houses()) + " houses"
 
-
 func _on_Spatial_mouse_entered():
   set_hover_text()
   Statistics.show_desciption = true
   hovered = true
-
 
 func _on_Spatial_mouse_exited():
   Statistics.show_desciption = false
